9ffbe8f6f6212ac845915e292094a23b5f25bbf3
Patricio Salazar <psalazar@empire-medical.com>
2024-07-21 20:56:55 -0700

    update vite config settings

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     // adding this to allow visiting from other devices
     server: {
-        host: '0.0.0.0',
-        port: 8001,
+        host: "0.0.0.0",
+        port: 8002,
         hmr: {
-            host: '192.168.1.89',
+            // host: '192.168.1.93', // a previous setting
+            host: "0.0.0.0",
         },
     },
 });
