31e8a5127dc50196468d353b63c69be599c77d12
Patricio Salazar <psalazar@empire-medical.com>
2024-08-30 19:18:41 -0700

    commenting out port setting in vite config for production

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    // adding this to allow visiting from other devices
     server: {
         host: "0.0.0.0",
         https: true,
-        port: 8002,
+        // port: 8002,
         hmr: {
-            // host: '192.168.1.93', // a previous setting
-            host: "bible-timeline-production.up.railway.app",
+            // host: '192.168.1.93', // previous
+            host: "0.0.0.0",
         },
     },
 });
